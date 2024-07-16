5f5318c7bd69665fd96d88f562192feb163d592a
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-07-16 17:49:30 +0200

    Ejercicio petición

--- ejercicio21_superejercicio1/cliente-http-xmlhttprequest.js
+++ ejercicio21_superejercicio1/cliente-http-xmlhttprequest.js
@@ -2,15 +2,37 @@ function getVideojuegos(url, port, recurso) {
     const client = new XMLHttpRequest();
 
     client.addEventListener("readystatechange", () => {
-        const isDone = client.readyState === 4;
-        const isOk = client.status === 200;
+        const isDone = client.readyState === 4;//Ha terminado
+        const isOk = client.status === 200;//OK
+        console.error(client.status);
 
         if (isDone && isOk) {
-            console.log(client.responseText);
+            mostrarDatos(client.responseText);
+        } else if (isDone && !isOk) {
+            mostrarPaginaError(client.status);
         }
     });
     client.open("GET", `${url}:${port}/${recurso}`);
     client.send();
-}   
+}  
 
-getVideojuegos("http://localhost",5501,"datos.json");
+function mostrarDatos(datos)
+{
+    let juegos = JSON.parse(datos);
+    juegos.forEach(juego => {
+        let ficha = `<div class="juego">
+            <div class="titulo">${juego.titulo}</div>
+            <div class="plataforma">${juego.plataforma}</div>
+            <div class="imagen">
+                <img src="${juego.caratula}">
+            </div>
+        </div>`;
+    document.querySelector("#juegos").innerHTML+=ficha; 
+    });   
+}
+
+function mostrarPaginaError(codigo){
+    document.querySelector("#juegos").innerHTML=`<h1>Ha ocurrido un error:${codigo}</h1>`;   
+}
+
+getVideojuegos("http://localhost",5501,"datosx.json");
